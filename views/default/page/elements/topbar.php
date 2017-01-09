74c1e828bc3314fb2b445a4aac49e1bb09505720
bart-j <bart@jeukendrup.nl>
2017-01-09 15:40:21 +0100

    improved compatibility with rijkshuisstijl

--- views/default/page/elements/topbar.php
+++ views/default/page/elements/topbar.php
@@ -1,20 +1,29 @@
 <?php
-
-if (elgg_is_logged_in()) {
-	// check if we need to show the topbar
-	if (elgg_get_config("navigation_bar_position") != "disabled") {
-		
-		// link back to main site.
-		$site = elgg_get_site_entity();
-		$site_name = $site->name;
-		$site_url = elgg_get_site_url();
-		
-		
-		echo "<h1>";
-		echo elgg_view("output/url", array("text" => $site->name, "title" => $site->name, "href" => $site->url, "class"=> "subsite-manager-topbar-logo"));
-		echo "</h1>";
-		
-		echo elgg_view("search/search_box");
-		echo elgg_view("page/elements/topbar/account");
+if (elgg_is_active_plugin("rijkshuisstijl")) {
+	if (elgg_is_logged_in()) {
+		echo elgg_view("subsite_manager/account/dropdown");	
+	} else {
+		echo elgg_view("core/account/login_button");
 	}
+} else {
+	if (!elgg_is_logged_in()) {
+		return;
+	}
+
+	// link back to main site.
+	$site = elgg_get_site_entity();
+	$site_name = $site->name;
+	$site_url = elgg_get_site_url();
+		
+	echo "<h1>";
+	echo elgg_view("output/url", array(
+		"text" => $site->name, 
+		"title" => $site->name,
+		"href" => $site->url, 
+		"class"=> "subsite-manager-topbar-logo"
+	));
+	echo "</h1>";
+
+	echo elgg_view("search/search_box");
+	echo elgg_view("page/elements/topbar/account");
 }
