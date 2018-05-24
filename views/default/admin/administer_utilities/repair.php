7c916160598b59379472c85ea844858989586e93
Bart Jeukendrup <bart@jeukendrup.nl>
2018-05-24 17:43:12 +0200

    Added ability to repair hidden groups

---
+++ views/default/admin/administer_utilities/repair.php
@@ -0,0 +1,10 @@
+<p>
+    <?php echo elgg_echo("admin:administer_utilities:repair:description"); ?>
+</p>
+
+<?php
+echo elgg_view_module(
+    "inline",
+    elgg_echo("pleio:hidden_groups"),
+    elgg_view("admin/administer_utilities/repair/hidden_groups")
+);
